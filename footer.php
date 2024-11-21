e6b6f34fe6d4e9c0ddd56da3d6b2bf23007a6069
Truong27301 <truongdhqn273@gmail.com>
2024-11-21 09:00:38 +0700

    template project

---
+++ footer.php
@@ -0,0 +1,94 @@
+<footer style="margin-left:-6%;margin-right:-6%;">
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-sm-1 col-md-1 col-lg-1">
+              </div>
+              <div class="col-sm-7 col-md-5 col-lg-5">
+                  <div class="row text-center">
+                      <h2>Let's Get In Touch!</h2>
+                      <hr class="primary">
+                      <p>Still Confused? Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                  </div>
+                  <div class="row">
+                      <div class="col-md-6 text-center">
+                          <span class="glyphicon glyphicon-earphone"></span>
+                          <p>123-456-6789</p>
+                      </div>
+                      <div class="col-md-6 text-center">
+                          <span class="glyphicon glyphicon-envelope"></span>
+                          <p>BookStore@gmail.com</p>
+                      </div>
+                  </div>
+              </div>
+              <div class="hidden-sm-down col-md-2 col-lg-2">
+              </div>
+              <div class="col-sm-4 col-md-3 col-lg-3 text-center">
+                  <h2 style="color:#D67B22;">Follow Us At</h2>
+                  <div>
+                      <a href="https://twitter.com/strandbookstore">
+                      <img title="Twitter" alt="Twitter" src="img/social/twitter.png" width="35" height="35" />
+                      </a>
+                      <a href="https://www.linkedin.com/company/strand-book-store">
+                      <img title="LinkedIn" alt="LinkedIn" src="img/social/linkedin.png" width="35" height="35" />
+                      </a>
+                      <a href="https://www.facebook.com/strandbookstore/">
+                      <img title="Facebook" alt="Facebook" src="img/social/facebook.png" width="35" height="35" />
+                      </a>
+                      <a href="https://plus.google.com/111917722383378485041">
+                      <img title="google+" alt="google+" src="img/social/google.jpg" width="35" height="35" />
+                      </a>
+                      <a href="https://www.pinterest.com/strandbookstore/">
+                      <img title="Pinterest" alt="Pinterest" src="img/social/pinterest.jpg" width="35" height="35" />
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
+
+<div class="container">
+  <!-- Trigger the modal with a button -->
+  <button type="button" id="query_button" class="btn btn-lg" data-toggle="modal" data-target="#query">Ask query</button>
+  <!-- Modal -->
+  <div class="modal fade" id="query" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="modal-header text-center">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Ask your query here</h4>
+          </div>
+          <div class="modal-body">           
+                    <form method="post" action="query.php" class="form" role="form">
+                        <div class="form-group">
+                             <label class="sr-only" for="name">Name</label>
+                             <input type="text" class="form-control"  placeholder="Your Name" name="sender" required>
+                        </div>
+                        <div class="form-group">
+                             <label class="sr-only" for="email">Email</label>
+                             <input type="email" class="form-control" placeholder="abc@gmail.com" name="senderEmail" required>
+                        </div>
+                        <div class="form-group">
+                             <label class="sr-only" for="query">Message</label>
+                             <textarea class="form-control" rows="5" cols="30" name="message" placeholder="Your Query" required></textarea>
+                        </div>
+                        <div class="form-group">
+                              <button type="submit" name="submit" value="query" class="btn btn-block">
+                                                              Send Query
+                               </button>
+                        </div>
+                    </form>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+      </div>
+    </div>  
+  </div>
+</div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>	
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "header.php"; ?>
+
+<?php include "top.php"; ?>
+
+<?php include "new_product.php" ?>
+
+<?php include "author.php" ?>
+
+<?php include "footer.php" ?>
