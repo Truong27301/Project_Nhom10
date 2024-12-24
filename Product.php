5783cf932ec113e49cf4db092b53a4fb27c9f0f7
Truong27301 <truongdhqn273@gmail.com>
2024-12-24 09:06:27 +0700

    tổng hợp code

--- Product.php
+++ Product.php
@@ -17,12 +17,16 @@ if(!isset($_SESSION['user']))
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/my.css" rel="stylesheet">
+
     <style>
-        #books {margin-bottom: 50px;}
-        @media only screen and (width: 768px) { body{margin-top:150px;}}
-        #books .row{margin-top:30px;margin-bottom:30px;font-weight:800;}
+   
+    #books {margin-bottom:50px;}
+
+     @media only screen and (width: 767px) { body{margin-top:150px;}}
+        #books .row{margin-top:20px;margin-bottom:10px;font-weight:800;}
         @media only screen and (max-width: 760px) { #books .row{margin-top:10px;}}
-   </style>
+    </style>
+
 </head>
 <body>
 
@@ -56,53 +60,58 @@ if(!isset($_SESSION['user']))
       </div><!-- /.container-fluid -->
     </nav>
 
-
     <div id="top" >
         <div id="searchbox" class="container-fluid" style="width:112%;margin-left:-6%;margin-right:-6%;">
             <div>
                 <form role="search" action="Result.php" method="post">
-                    <input type="text" class="form-control" name="keyword" placeholder="Search for a Book , Author Or Category" style="width:80%;margin:20px 10% 20px 10%;">
+                    <input type="text" name="keyword" class="form-control" placeholder="Search for a Book , Author Or Category" style="width:80%;margin:20px 10% 20px 10%;">
                 </form>
             </div>
         </div>
 
     <?php
-    include "dbconnect.php";
-        if(isset($_GET['value']))
+    include "models/dbconnect.php";
+    if(isset($_GET['value']))
         {  
-           $_SESSION['author']=$_GET['value'];
+           $_SESSION['category']=$_GET['value'];
         }
-    $author=$_SESSION['author'];
+    $category=$_SESSION['category'];
     if(isset($_POST['sort']))
     {
         if($_POST['sort']=="price")
-                {   $query = "SELECT * FROM products WHERE Author='$author' ORDER BY Price";
+                {   $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Price";
                     $result = mysqli_query ($con,$query)or die(mysqli_error($con));
+                    ?>
+                       <script type="text/javascript">
+                          document.getElementById('select').Selected=$_POST['sort'];
+                       </script>
+                    <?php
                 }
         else
         if($_POST['sort']=="priceh")
-                {   $query = "SELECT * FROM products WHERE Author='$author' ORDER BY Price DESC";
+                {   $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Price DESC";
                     $result = mysqli_query ($con,$query)or die(mysqli_error($con));
                 }
         else
         if($_POST['sort']=="discount")
-                {   $query = "SELECT * FROM products WHERE Author='$author' ORDER BY Discount DESC";
+                {   $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Discount DESC";
                     $result = mysqli_query ($con,$query)or die(mysqli_error($con));
                 }
         else
         if($_POST['sort']=="discountl")
-                {   $query = "SELECT * FROM products WHERE Author='$author' ORDER BY Discount";
+                {   $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Discount";
                     $result = mysqli_query ($con,$query)or die(mysqli_error($con));
                 }
     } 
     else   
-                  $query = "SELECT * FROM products WHERE Author='$author'";
-                  $result = mysqli_query ($con,$query)or die(mysql_error());
+            {   $query = "SELECT * FROM products WHERE Category='$category'";
+                $result = mysqli_query ($con,$query)or die(mysqli_error($con));
+            } 
     $i=0;
     echo '<div class="container-fluid" id="books">
         <div class="row">
           <div class="col-xs-12 text-center" id="heading">
-                 <h2 style="color:#D67B22;text-transform:uppercase;margin-bottom:0px;"> '. $author .' STORE </h2>
+                 <h2 style="color:rgb(228, 55, 25);text-transform:uppercase;margin-bottom:0px;"> '. $category .' STORE </h2>
            </div>
         </div>      
         <div class="container fluid">
@@ -110,7 +119,7 @@ if(!isset($_SESSION['user']))
                   <div class="col-sm-5 col-sm-offset-6 col-md-5 col-md-offset-7 col-lg-4 col-lg-offset-8">
                        <form action="';echo $_SERVER['PHP_SELF'];echo'" method="post" class="pull-right">
                            <label for="sort">Sort by &nbsp: &nbsp</label>
-                            <select name="sort" onchange="form.submit()">
+                            <select name="sort" id="select" onchange="form.submit()">
                                 <option value="default" name="default"  selected="selected">Select</option>
                                 <option value="price" name="price">Low To High Price </option>
                                 <option value="priceh" name="priceh">Highest To Lowest Price </option>
@@ -159,4 +168,14 @@ if(!isset($_SESSION['user']))
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
 </body>
-</html>		
+</html>	
+<!--	
+<script>
+$('#my_select').change(function() {   
+   // assign the value to a variable, so you can test to see if it is working
+    var selectVal = $('#my_select :selected').val();
+    alert(selectVal);
+});
+</script>
+
+-->
