a45e125a7eeaf5810178e9fd9ed8da0e7ba662b9
Truong27301 <truongdhqn273@gmail.com>
2024-12-26 08:35:46 +0700

    Fix bug update

--- Product.php
+++ Product.php
@@ -70,112 +70,118 @@ if(!isset($_SESSION['user']))
         </div>
 
     <?php
-    include "models/dbconnect.php";
-    if(isset($_GET['value']))
-        {  
-           $_SESSION['category']=$_GET['value'];
-        }
-    $category=$_SESSION['category'];
-    if(isset($_POST['sort']))
-    {
-        if($_POST['sort']=="price")
-                {   $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Price";
-                    $result = mysqli_query ($con,$query)or die(mysqli_error($con));
-                    ?>
-                       <script type="text/javascript">
-                          document.getElementById('select').Selected=$_POST['sort'];
-                       </script>
-                    <?php
-                }
-        else
-        if($_POST['sort']=="priceh")
-                {   $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Price DESC";
-                    $result = mysqli_query ($con,$query)or die(mysqli_error($con));
-                }
-        else
-        if($_POST['sort']=="discount")
-                {   $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Discount DESC";
-                    $result = mysqli_query ($con,$query)or die(mysqli_error($con));
-                }
-        else
-        if($_POST['sort']=="discountl")
-                {   $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Discount";
-                    $result = mysqli_query ($con,$query)or die(mysqli_error($con));
-                }
-    } 
-    else   
-            {   $query = "SELECT * FROM products WHERE Category='$category'";
-                $result = mysqli_query ($con,$query)or die(mysqli_error($con));
-            } 
-    $i=0;
-    echo '<div class="container-fluid" id="books">
+include "models/dbconnect.php";
+
+// Set up pagination variables
+$products_per_page = 4; // số sản phẩm mỗi trang
+if(isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1; // Mặc định là trang 1
+}
+
+$start_from = ($page - 1) * $products_per_page; // Tính offset
+
+if(isset($_GET['value'])) {  
+    $_SESSION['category'] = $_GET['value'];
+}
+$category = $_SESSION['category'];
+
+// Query sản phẩm với phân trang
+if(isset($_POST['sort'])) {
+    $sort_by = $_POST['sort'];
+    switch($sort_by) {
+        case "price":
+            $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Price LIMIT $start_from, $products_per_page";
+            break;
+        case "priceh":
+            $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Price DESC LIMIT $start_from, $products_per_page";
+            break;
+        case "discount":
+            $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Discount DESC LIMIT $start_from, $products_per_page";
+            break;
+        case "discountl":
+            $query = "SELECT * FROM products WHERE Category='$category' ORDER BY Discount LIMIT $start_from, $products_per_page";
+            break;
+        default:
+            $query = "SELECT * FROM products WHERE Category='$category' LIMIT $start_from, $products_per_page";
+    }
+} else {
+    $query = "SELECT * FROM products WHERE Category='$category' LIMIT $start_from, $products_per_page";
+}
+
+$result = mysqli_query($con, $query) or die(mysqli_error($con));
+
+// Display products
+echo '<div class="container-fluid" id="books">
         <div class="row">
-          <div class="col-xs-12 text-center" id="heading">
-                 <h2 style="color:rgb(228, 55, 25);text-transform:uppercase;margin-bottom:0px;"> '. $category .' STORE </h2>
-           </div>
-        </div>      
+            <div class="col-xs-12 text-center" id="heading">
+                <h2 style="color:rgb(228, 55, 25);text-transform:uppercase;margin-bottom:0px;"> '. $category .' STORE </h2>
+            </div>
+        </div>
         <div class="container fluid">
-             <div class="row">
-                  <div class="col-sm-5 col-sm-offset-6 col-md-5 col-md-offset-7 col-lg-4 col-lg-offset-8">
-                       <form action="';echo $_SERVER['PHP_SELF'];echo'" method="post" class="pull-right">
-                           <label for="sort">Sort by &nbsp: &nbsp</label>
-                            <select name="sort" id="select" onchange="form.submit()">
-                                <option value="default" name="default"  selected="selected">Select</option>
-                                <option value="price" name="price">Low To High Price </option>
-                                <option value="priceh" name="priceh">Highest To Lowest Price </option>
-                                <option value="discountl" name="discountl">Low To High Discount </option>
-                                <option value="discount" name="discount">Highest To Lowest Discount</option>
-                            </select>
-                       </form>
-                  </div>
-              </div>
+            <div class="row">
+                <div class="col-sm-5 col-sm-offset-6 col-md-5 col-md-offset-7 col-lg-4 col-lg-offset-8">
+                    <form action="';echo $_SERVER['PHP_SELF'];echo'" method="post" class="pull-right">
+                        <label for="sort">Sort by &nbsp: &nbsp</label>
+                        <select name="sort" id="select" onchange="form.submit()">
+                            <option value="default" selected="selected">Select</option>
+                            <option value="price">Low To High Price </option>
+                            <option value="priceh">Highest To Lowest Price </option>
+                            <option value="discountl">Low To High Discount </option>
+                            <option value="discount">Highest To Lowest Discount</option>
+                        </select>
+                    </form>
+                </div>
+            </div>
         </div>';
 
-        if(mysqli_num_rows($result) > 0) 
-        {   
-            while($row = mysqli_fetch_assoc($result)) 
-            {
-            $path="img/books/" .$row['PID'].".jpg";
-            $description="description.php?ID=".$row["PID"];
-            if($i%4==0)
-            echo '<div class="row">';
-            echo'
-               <a href="'.$description.'">
+if(mysqli_num_rows($result) > 0) {
+    $i = 0;
+    while($row = mysqli_fetch_assoc($result)) {
+        $path = "img/books/" . $row['PID'] . ".jpg";
+        $description = "description.php?ID=" . $row["PID"];
+        if($i % 4 == 0) echo '<div class="row">';
+        echo '
+            <a href="' . $description . '">
                 <div class="col-sm-6 col-md-3 col-lg-3 text-center">
-                    <div class="book-block" style="border :3px solid #DEEAEE;">
-                        <img class="book block-center img-responsive" src="'.$path.'">
+                    <div class="book-block" style="border: 3px solid #DEEAEE;">
+                        <img class="book block-center img-responsive" src="' . $path . '">
                         <hr>
-                         ' . $row["Title"] . '<br>
-                        ' . $row["Price"] .'  &nbsp
-                        <span style="text-decoration:line-through;color:#828282;"> ' . $row["MRP"] .' </span>
-                        <span class="label label-warning">'. $row["Discount"] .'%</span>
+                        ' . $row["Title"] . '<br>
+                        ' . $row["Price"] . ' &nbsp;
+                        <span style="text-decoration:line-through;color:#828282;"> ' . $row["MRP"] . ' </span>
+                        <span class="label label-warning">' . $row["Discount"] . '%</span>
                     </div>
                 </div>
-                
-               </a> ';
-            $i++;
-            if($i%4==0)
-            echo '</div>';
-            }
-        }
-    echo '</div>';
-    ?>
+            </a>';
+        $i++;
+        if($i % 4 == 0) echo '</div>';
+    }
+}
 
+echo '</div>';
 
+// Pagination
+// Query to get total number of products
+$query_total = "SELECT COUNT(*) AS total FROM products WHERE Category='$category'";
+$result_total = mysqli_query($con, $query_total);
+$row_total = mysqli_fetch_assoc($result_total);
+$total_products = $row_total['total'];
+$total_pages = ceil($total_products / $products_per_page);
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+echo '<div class="text-center">';
+for($i = 1; $i <= $total_pages; $i++) {
+    echo '<a href="product.php?page=' . $i . '" class="btn btn-default">' . $i . '</a> ';
+}
+echo '</div>';
+
+?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>	
-<!--	
-<script>
-$('#my_select').change(function() {   
-   // assign the value to a variable, so you can test to see if it is working
-    var selectVal = $('#my_select :selected').val();
-    alert(selectVal);
-});
-</script>
 
--->
